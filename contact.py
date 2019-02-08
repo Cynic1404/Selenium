d24947b1c971ac311deedcf5bf187eedbe9bb365
Cynic1404 <nekhaenok@gmail.com>
2019-02-07 17:52:32 -0800

    UnitTest. Tests for contacts and groups.

---
+++ contact.py
@@ -0,0 +1,6 @@
+class Contact:
+    def __init__(self, name, last_name, mobile_phone, company):
+        self.name = name
+        self.last_name = last_name
+        self.mobile_phone = mobile_phone
+        self.company = company
