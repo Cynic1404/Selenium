06f827921fc0124f1e357b89251bacd3766705ca
Cynic1404 <nekhaenok@gmail.com>
2019-02-19 15:02:02 -0800

    login and logout functons moved to fixture(conftest.py) and removed from all test methods

--- fixture/contact.py
+++ fixture/contact.py
@@ -14,6 +14,12 @@ class ContactHelper:
 
     def delete_first_contact(self):
         wd = self.app.wd
+        self.open_contacts_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath('//*[@id="content"]/form[2]/div[2]/input').click()
         wd.switch_to_alert().accept()
+
+    def open_contacts_page(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath('//*[@id="nav"]/ul/li[1]/a').click()
+
--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,18 +1,8 @@
-
 from model.contact import Contact
 
-
-
-
 def test_add_contact(app):
-        app.open_home_page()
-        app.session.login(username="admin", password="secret")
         app.contact.create(Contact(name="First contact", last_name="last", mobile_phone="mobile", company="company"))
-        app.session.logout()
-
 def test_add_emplty_contact(app):
-        app.open_home_page()
-        app.session.login(username="admin", password="secret")
         app.contact.create(Contact(name="", last_name="", mobile_phone="", company=""))
-        app.session.logout()
+
 
